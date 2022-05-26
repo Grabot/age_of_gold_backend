70415f6028cf4b510a548b7980eb0fd6287af9dc
Grabot <SanderKools@gmail.com>
2022-05-26 11:53:25 +0200

    create hexagons and tiles for debugging

--- routes/map/hexagon_route.py
+++ routes/map/hexagon_route.py
@@ -3,6 +3,12 @@ from schema import SchemaHexagon
 from app import app
 
 
+@app.post("/hexagon/")
+async def create_hexagon(hexagon: SchemaHexagon):
+    hexagon_id = await Hexagon.create(**hexagon.dict())
+    return {"hexagon_id": hexagon_id}
+
+
 @app.get("/hexagon/{id}", response_model=SchemaHexagon)
 async def get_hexagon(id: int):
     hexagon = await Hexagon.get(id)
--- routes/map/tile_route.py
+++ routes/map/tile_route.py
@@ -3,6 +3,12 @@ from schema import SchemaTile
 from app import app
 
 
+@app.post("/tile/")
+async def create_tile(tile: SchemaTile):
+    tile_id = await Tile.create(**tile.dict())
+    return {"tile_id": tile_id}
+
+
 @app.get("/tile/{id}", response_model=SchemaTile)
 async def get_tile(id: int):
     tile = await Tile.get(id)
