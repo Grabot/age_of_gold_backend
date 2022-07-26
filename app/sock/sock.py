33cdedbeb1fd1ae06329c33b859312e26390d27d
Grabot <SanderKools@gmail.com>
2022-07-26 11:10:35 +0200

    some code refactoring

--- app/sock/sock.py
+++ app/sock/sock.py
@@ -49,12 +49,23 @@ class NamespaceSock(Namespace):
         user_id = data["userId"]
         room = "room_%s" % user_id
         if q is not None and r is not None and s is not None:
-            # q is on the other side, but r is still good.
-            if q > map_size and (-4 <= r <= 4):
+            # If the hex is out of the map bounds we want it to loop around
+            if q < -map_size or q > map_size or r < -map_size or r > map_size:
                 wrap_q = 0
                 while q > map_size:
                     q = q - (2 * map_size + 1)
                     wrap_q += 1
+                while q < -map_size:
+                    q = q + (2 * map_size + 1)
+                    wrap_q -= 1
+                wrap_r = 0
+                while r > map_size:
+                    r = r - (2 * map_size + 1)
+                    wrap_r += 1
+                while r < -map_size:
+                    r = r + (2 * map_size + 1)
+                    wrap_r -= 1
+
                 s = (q + r) * -1
                 print("wraparound test! q: {} r: {} s: {}   wrap_q: {}".format(q, r, s, wrap_q))
                 hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
@@ -62,172 +73,19 @@ class NamespaceSock(Namespace):
                 return_hexagon = hexagon.serialize
                 return_hexagon["wraparound"] = {
                     "q": wrap_q,
-                    "r": 0
-                }
-                emit("send_hexagon_success", return_hexagon, room=room)
-                return
-            elif q < -map_size and (-4 <= r <= 4):
-                wrap_q = 0
-                while q < -map_size:
-                    q = q + (2 * map_size + 1)
-                    wrap_q -= 1
-                s = (q + r) * -1
-                print("wraparound test 5! q: {} r: {} s: {}   wrap_q: {}".format(q, r, s, wrap_q))
-                hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
-                # We will add a wraparound indicator
-                return_hexagon = hexagon.serialize
-                return_hexagon["wraparound"] = {
-                    "q": wrap_q,
-                    "r": 0
-                }
-                emit("send_hexagon_success", return_hexagon, room=room)
-                return
-            elif (-4 <= q <= 4) and r > map_size:
-                wrap_r = 0
-                while r > map_size:
-                    r = r - (2 * map_size + 1)
-                    wrap_r += 1
-                s = (q + r) * -1
-                print("wraparound test 2! q: {} r: {} s: {}   wrap r: {}".format(q, r, s, wrap_r))
-                hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
-                return_hexagon = hexagon.serialize
-                return_hexagon["wraparound"] = {
-                    "q": 0,
-                    "r": wrap_r
-                }
-                emit("send_hexagon_success", return_hexagon, room=room)
-                return
-            elif (-4 <= q <= 4) and r < -map_size:
-                wrap_r = 0
-                while r < -map_size:
-                    r = r + (2 * map_size + 1)
-                    wrap_r -= 1
-                s = (q + r) * -1
-                print("wraparound test 2! q: {} r: {} s: {}   wrap r: {}".format(q, r, s, wrap_r))
-                hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
-                return_hexagon = hexagon.serialize
-                return_hexagon["wraparound"] = {
-                    "q": 0,
-                    "r": wrap_r
-                }
-                emit("send_hexagon_success", return_hexagon, room=room)
-                return
-            elif q > map_size and r > map_size:
-                wrap_q = 0
-                while q > map_size:
-                    q = q - (2 * map_size + 1)
-                    wrap_q += 1
-                wrap_r = 0
-                while r > map_size:
-                    r = r - (2 * map_size + 1)
-                    wrap_r += 1
-                s = (q + r) * -1
-                print("wraparound test 3! q: {} r: {} s: {}  wrap_q: {}   wrap_r: {}".format(q, r, s, wrap_q, wrap_r))
-                hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
-                return_hexagon = hexagon.serialize
-                return_hexagon["wraparound"] = {
-                    "q": wrap_q,
-                    "r": wrap_r
-                }
-                emit("send_hexagon_success", return_hexagon, room=room)
-                return
-            elif q > map_size and r < -map_size:
-                wrap_q = 0
-                while q > map_size:
-                    q = q - (2 * map_size + 1)
-                    wrap_q += 1
-                wrap_r = 0
-                while r < -map_size:
-                    r = r + (2 * map_size + 1)
-                    wrap_r -= 1
-                s = (q + r) * -1
-                print("wraparound test 3! q: {} r: {} s: {}  wrap_q: {}   wrap_r: {}".format(q, r, s, wrap_q, wrap_r))
-                hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
-                return_hexagon = hexagon.serialize
-                return_hexagon["wraparound"] = {
-                    "q": wrap_q,
-                    "r": wrap_r
-                }
-                emit("send_hexagon_success", return_hexagon, room=room)
-                return
-            elif q < -map_size and r > map_size:
-                wrap_q = 0
-                while q < -map_size:
-                    q = q + (2 * map_size + 1)
-                    wrap_q -= 1
-                wrap_r = 0
-                while r > map_size:
-                    r = r - (2 * map_size + 1)
-                    wrap_r += 1
-                s = (q + r) * -1
-                print("wraparound test 3! q: {} r: {} s: {}  wrap_q: {}   wrap_r: {}".format(q, r, s, wrap_q, wrap_r))
-                hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
-                return_hexagon = hexagon.serialize
-                return_hexagon["wraparound"] = {
-                    "q": wrap_q,
-                    "r": wrap_r
-                }
-                emit("send_hexagon_success", return_hexagon, room=room)
-                return
-            elif q < -map_size and r < -map_size:
-                wrap_q = 0
-                while q < -map_size:
-                    q = q + (2 * map_size + 1)
-                    wrap_q -= 1
-                wrap_r = 0
-                while r < -map_size:
-                    r = r + (2 * map_size + 1)
-                    wrap_r -= 1
-                s = (q + r) * -1
-                print("wraparound test 3! q: {} r: {} s: {}  wrap_q: {}   wrap_r: {}".format(q, r, s, wrap_q, wrap_r))
-                hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
-                return_hexagon = hexagon.serialize
-                return_hexagon["wraparound"] = {
-                    "q": wrap_q,
                     "r": wrap_r
                 }
                 emit("send_hexagon_success", return_hexagon, room=room)
                 return
             else:
+                # The hex is within the map bounds so retrieve it
                 hexagon = Hexagon.query.filter_by(q=q, r=r, s=s).first()
-            if hexagon is None:
-                emit("send_hexagon_fail", 'hexagon getting failed', room=room)
-            else:
-                emit("send_hexagon_success", hexagon.serialize, room=room)
+                if hexagon is None:
+                    emit("send_hexagon_fail", 'hexagon getting failed', room=room)
+                else:
+                    emit("send_hexagon_success", hexagon.serialize, room=room)
         else:
             emit("send_hexagon_fail", 'hexagon getting failed', room=room)
-
-    # noinspection PyMethodMayBeStatic
-    def on_get_hexagons_q(self, data):
-        q_begin = data["q_begin"]
-        q_end = data["q_end"]
-        r_row = data["r_row"]
-        user_id = data["userId"]
-        room = "room_%s" % user_id
-        if q_begin is not None and q_end is not None and r_row is not None:
-            hexagons = Hexagon.query.filter(Hexagon.q.between(q_begin, q_end)).filter(Hexagon.r == r_row).all()
-            return_hexagons = []
-            for hexagon in hexagons:
-                return_hexagons.append(hexagon.serialize)
-            emit("send_hexagons_success", return_hexagons, room=room)
-
-    # noinspection PyMethodMayBeStatic
-    def on_get_hexagons_r(self, data):
-        r_begin = data["r_begin"]
-        r_end = data["r_end"]
-        q_row = data["q_row"]
-        user_id = data["userId"]
-        room = "room_%s" % user_id
-        if r_begin is not None and r_end is not None and q_row is not None:
-            hexagons = Hexagon.query.filter(Hexagon.r.between(r_begin, r_end)).filter(Hexagon.q == q_row).all()
-            return_hexagons = []
-            for hexagon in hexagons:
-                return_hexagons.append(hexagon.serialize)
-            emit("send_hexagons_success", return_hexagons, room=room)
-
-    # # noinspection PyMethodMayBeStatic
-    # def on_message(self, data):
-    #     send_message(data)
 
 
 socks.on_namespace(NamespaceSock('/api/v1.0/sock'))
