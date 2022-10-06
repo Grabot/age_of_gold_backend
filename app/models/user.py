0148652816aed6215410364b2de520f3af4d366d
Grabot <SanderKools@gmail.com>
2022-10-07 00:28:23 +0200

    adding some basic user profile and more information

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,8 @@ from passlib.apps import custom_app_context as pwd_context
 from app import db
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
+from datetime import datetime
 
 
 @login.user_loader
@@ -18,12 +20,18 @@ class User(UserMixin, db.Model):
     username = db.Column(db.Text, index=True, unique=False)
     email = db.Column(db.Text, index=True, unique=True)
     password_hash = db.Column(db.Text)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
 
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     @property
     def serialize(self):
