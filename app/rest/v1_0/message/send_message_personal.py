9183ad38649cc69f85ddc723d56f50583292c20a
Grabot <SanderKools@gmail.com>
2023-03-26 17:56:39 +0200

    fixed some personal chat functionality

--- app/rest/v1_0/message/send_message_personal.py
+++ app/rest/v1_0/message/send_message_personal.py
@@ -2,7 +2,10 @@ from flask import request, make_response
 from flask_restful import Api
 from flask_restful import Resource
 from flask_socketio import emit
+from sqlalchemy import func
+
 from app.models.post import Post
+from app.models.user import User
 from app.rest import app_api
 from app import db, DevelopmentConfig
 from app.rest.rest_util import get_failed_response
@@ -28,18 +31,25 @@ class SendMessagePersonal(Resource):
         if auth_token == '':
             return get_failed_response("an error occurred")
 
-        user = check_token(auth_token)
-        if not user:
+        from_user = check_token(auth_token)
+        if not from_user:
             return get_failed_response("an error occurred")
 
         message_body = json_data["message"]
-        to_user = json_data["to_user"]
+        to_user = json_data["to_user"].lower()
+        user_send = User.query.filter(func.lower(User.username) == func.lower(to_user)).first()
+        if not user_send:
+            return get_failed_response("user not found")
+
+        room_from = "room_%s" % from_user.id
+        room_to = "room_%s" % user_send.id
         socket_response = {
-            "user_name": user.username,
+            "user_name": from_user.username,
             "message": message_body
         }
 
-        emit("send_message_personal", socket_response, broadcast=True, namespace=DevelopmentConfig.API_SOCK_NAMESPACE)
+        emit("send_message_personal", socket_response, room=room_from, namespace=DevelopmentConfig.API_SOCK_NAMESPACE)
+        emit("send_message_personal", socket_response, room=room_to, namespace=DevelopmentConfig.API_SOCK_NAMESPACE)
 
         send_message_response = make_response({
             'result': True,
--- app/sock/sock.py
+++ app/sock/sock.py
@@ -33,10 +33,20 @@ class NamespaceSock(Namespace):
     # noinspection PyMethodMayBeStatic
     def on_join(self, data):
         user_id = data["userId"]
-        room = "room_%s" % user_id
-        # print("joined room: %s" % room)
-        join_room(room)
-        emit("message_event", 'User has entered room %s' % room, room=room, namespace='/api/v1.0/sock')
+        if user_id != -1:
+            room = "room_%s" % user_id
+            print("joined room: %s" % room)
+            join_room(room)
+            emit("message_event", 'User has entered room %s' % room, room=room, namespace='/api/v1.0/sock')
+
+    # noinspection PyMethodMayBeStatic
+    def on_leave(self, data):
+        user_id = data["userId"]
+        if user_id != -1:
+            room = "room_%s" % user_id
+            leave_room(room)
+            # print("left room %s" % room)
+            emit("message_event", 'User has left room %s' % room, room=request.sid)
 
     # noinspection PyMethodMayBeStatic
     def on_join_hex(self, data):
@@ -61,14 +71,6 @@ class NamespaceSock(Namespace):
         leave_room(room)
         # print("left hex room: %s" % room)
         emit("message_event", 'User has left hex room %s' % room, room=request.sid)
-
-    # noinspection PyMethodMayBeStatic
-    def on_leave(self, data):
-        user_id = data["userId"]
-        room = "room_%s" % user_id
-        leave_room(room)
-        # print("left room %s" % room)
-        emit("message_event", 'User has left room %s' % room, room=request.sid)
 
     # # noinspection PyMethodMayBeStatic
     # def on_send_message(self, data):
