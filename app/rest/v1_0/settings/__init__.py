192636a11c76ec617633a1dd772a0f2b37cbd563
Grabot <SanderKools@gmail.com>
2023-04-07 21:37:21 +0200

    adding change password

--- app/rest/v1_0/settings/__init__.py
+++ app/rest/v1_0/settings/__init__.py
@@ -1,2 +1,3 @@
 from app.rest.v1_0.settings import change_username
 from app.rest.v1_0.settings import change_avatar
+from app.rest.v1_0.settings import change_password
---
+++ app/rest/v1_0/settings/change_password.py
@@ -0,0 +1,51 @@
+from flask import request, make_response
+from flask_restful import Api
+from flask_restful import Resource
+from sqlalchemy import func
+
+from app import db
+from app.models.user import User
+from app.rest import app_api
+from app.rest.rest_util import get_failed_response
+from app.util.util import get_auth_token, check_token
+
+
+class ChangePassword(Resource):
+
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
+
+    # noinspection PyMethodMayBeStatic
+    def post(self):
+        print("Changing password")
+        json_data = request.get_json(force=True)
+        auth_token = get_auth_token(request.headers.get('Authorization'))
+        if auth_token == '':
+            return get_failed_response("Something went wrong")
+
+        user = check_token(auth_token)
+        if not user:
+            return get_failed_response("Something went wrong")
+
+        new_password = json_data["password"]
+        user.hash_password(new_password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        change_password_response = make_response({
+            'result': True,
+            'message': new_password,
+        }, 200)
+
+        return change_password_response
+
+
+api = Api(app_api)
+api.add_resource(ChangePassword, '/api/v1.0/change/password', endpoint='change_password')
