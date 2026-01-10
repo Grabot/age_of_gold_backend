5b2ddc74a9ffae5e341bc422be0cd10ae25e8038
Grabot <SanderKools@gmail.com>
2026-01-10 09:09:43 +0100

    revert for now

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,8 +3,5 @@
 from .user import User
 from .user_token import UserToken
 from .friend import Friend
-from .message import Message
-from .chat import Chat
-from .group import Group
 
-__all__ = ["User", "UserToken", "Friend", "Message", "Chat", "Group"]
+__all__ = ["User", "UserToken", "Friend"]
