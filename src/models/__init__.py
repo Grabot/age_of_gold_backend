7b17960f88fe4b9a5d83409cf51568160fd08623
Grabot <SanderKools@gmail.com>
2026-01-10 09:23:23 +0100

    added group object

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,5 +3,6 @@
 from .user import User
 from .user_token import UserToken
 from .friend import Friend
+from .group import Group
 
-__all__ = ["User", "UserToken", "Friend"]
+__all__ = ["User", "UserToken", "Friend", "Group"]
